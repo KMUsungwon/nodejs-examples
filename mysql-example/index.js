5046371bfb0a728bed8ce863b63681bd3da65f1d
websites <sungwontoto@kookmin.ac.kr>
2021-09-21 13:58:50 +0900

    edit connection info

--- mysql-example/index.js
+++ mysql-example/index.js
@@ -5,8 +5,8 @@ const app = express();
 const conn = {
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: 'sung980327',
+    user: 'username',
+    password: 'userPw',
     database: 'kleague', // your database to use
 }
 
